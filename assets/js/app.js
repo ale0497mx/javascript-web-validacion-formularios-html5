19c6db34218953e749f25e7e395eab9be10831f3
Aleandro_Resendiz <ale_resendiz321@hotmail.com>
2023-07-21 12:54:15 -0600

    clase04 completada finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 import { valida } from "./validaciones.js";
 
-const inputs = document.querySelectorAll("input");
-
-inputs.forEach( input => {
-    input.addEventListener("blur", (input) => {
-        valida(input.target);
+const inputs = document.querySelectorAll("input"); // todos los elementos de tipo input
+inputs.forEach(input => {
+    input.addEventListener("blur", (event) => {
+        valida(event.target);    
     });
 });
--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,3 +1,4 @@
+
 // const inputBornDate = document.querySelector('#birth');
 
 // inputBornDate.addEventListener('blur', (evento) => {
@@ -8,16 +9,39 @@ export function valida(input){
     if(validations[typeOfInput]){
         validations[typeOfInput](input);
     }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+    }
+}
+
+const errorMensajes = {
+    name: {
+        valueMissing: "Este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMissing: "el correo no es valido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patterMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    bornDate: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes ser mayor de edad para registrate"
+    }
 }
 
 const validations = {
-    nacimiento: input => validateBorn(input),
+    nacimiento: (input) => validateBorn(input),
 }
 
 function validateBorn(input){
     const userDate = new Date(input.value);
     let mensaje = '';
-    adult(userDate);
     if(!adult(userDate)){
         mensaje = 'Debes ser mayor de edad para registrate';
     }
@@ -28,6 +52,7 @@ function adult(date){
     const diffDates = new Date(
     date.getUTCFullYear() + 18, 
     date.getUTCMonth(), 
-    date.getUTCDate());
+    date.getUTCDate()
+    );
     return diffDates < actualDate;
 }
