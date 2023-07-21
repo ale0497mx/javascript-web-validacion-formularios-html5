4c5e023802d93be4a6e152df80a2287e6e7b35a2
Aleandro_Resendiz <ale_resendiz321@hotmail.com>
2023-07-21 10:27:48 -0600

    Curoso03 completado

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,1 +1,33 @@
-console.log("validacion");
+// const inputBornDate = document.querySelector('#birth');
+
+// inputBornDate.addEventListener('blur', (evento) => {
+//     validateBorn(evento.target);
+// });
+export function valida(input){
+    const typeOfInput = input.dataset.tipo;
+    if(validations[typeOfInput]){
+        validations[typeOfInput](input);
+    }
+}
+
+const validations = {
+    nacimiento: input => validateBorn(input),
+}
+
+function validateBorn(input){
+    const userDate = new Date(input.value);
+    let mensaje = '';
+    adult(userDate);
+    if(!adult(userDate)){
+        mensaje = 'Debes ser mayor de edad para registrate';
+    }
+    input.setCustomValidity(mensaje);
+}
+function adult(date){
+    const actualDate =  new Date();
+    const diffDates = new Date(
+    date.getUTCFullYear() + 18, 
+    date.getUTCMonth(), 
+    date.getUTCDate());
+    return diffDates < actualDate;
+}
